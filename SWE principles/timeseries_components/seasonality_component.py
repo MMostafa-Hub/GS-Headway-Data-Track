65d1ad6ef8caa74118d2c514c1c21c6c9380c807
Mohamed Mostafa <mmostafa.ibra21@gmail.com>
2023-09-01 20:42:33 +0300

    Implement Seasonality component

--- SWE principles/timeseries_components/seasonality_component.py
+++ SWE principles/timeseries_components/seasonality_component.py
@@ -1,21 +1,35 @@
 from component import Component
 import pandas as pd
+import numpy as np
+from enum import Enum
+
+
+class SeasonalityPeriod(Enum):
+    DAY = 1
+    WEEK = 7
+    MONTH = 30
+    QUARTER = 90
+    YEAR = 365
 
 
 class SeasonalityComponent(Component):
-    def __init__(
-        self, time_index: pd.DatetimeIndex, period: int, in_days: bool = True
-    ) -> None:
+    def __init__(self, time_index: pd.DatetimeIndex, period: SeasonalityPeriod) -> None:
         """Initialize the period of the seasonality component.
 
         Args:
-            period (int): the period of the seasonality component
-            in_days (bool, optional): Defaulting the period to be in days.
+            period (int): the period of the seasonality component in days
         """
         super().__init__(time_index)
         self.period = period
-        self.in_days = in_days
 
     @property
     def values(self) -> pd.Series:
-        return pd.Series()
+        """Create seasonality component for a time series."""
+        return pd.Series(np.sin(2 * np.pi * self.__total_period() / self.period.value))
+
+    def __total_period(self):
+        """Convert the total period to days or hours depending on the period"""
+        if self.period == SeasonalityPeriod.DAY:
+            return (self.time_index - self.time_index[0]).seconds / 3600
+        else:
+            return (self.time_index - self.time_index[0]).days
