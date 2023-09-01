8138831485e9e74d4f7f238d4c361fdbcc033c8d
Mohamed Mostafa <mmostafa.ibra21@gmail.com>
2023-09-01 16:35:10 +0300

    Modify the structure of Component class

--- SWE principles/timeseries_components/component.py
+++ SWE principles/timeseries_components/component.py
@@ -3,7 +3,16 @@ import pandas as pd
 
 
 class Component(ABC):
+    def __init__(self, time_series: pd.Series) -> None:
+        """Initialize the component.
+
+        Args:
+            time_series (pd.Series): the time series to create the component from
+        """
+        self.time_series = time_series
+
+    @property
     @abstractmethod
-    def add(self, time_series: pd.Series) -> pd.Series:
+    def values(self) -> pd.Series:
         """Add a component to a time series."""
         pass
