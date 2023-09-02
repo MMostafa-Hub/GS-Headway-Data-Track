d660e9157c35430556341457aeae1eb773fd44dc
Mohamed Mostafa <mmostafa.ibra21@gmail.com>
2023-09-02 17:55:31 +0300

    Add attributes to generate a time_index

--- SWE principles/timeseries/timeseries_generator.py
+++ SWE principles/timeseries/timeseries_generator.py
@@ -11,13 +11,25 @@ from functools import reduce
 class TimeSeriesGenerator:
     def __init__(
         self,
+        start_date: pd.Timestamp,
+        end_date: pd.Timestamp,
+        frequency: str,
         main_components: List[MainComponent],
         residual_components: List[ResidualComponent],
         multiplicative: bool = True,
     ) -> None:
+        """Initialize the time series generator."""
+        self.start_date = start_date
+        self.end_date = end_date
+        self.frequency = frequency
         self.multiplicative = multiplicative
         self.main_components = main_components
         self.residual_components = residual_components
+
+        # Creating the time index
+        self.time_index = pd.date_range(
+            start=self.start_date, end=self.end_date, freq=self.frequency
+        )
 
     def generate(self) -> pd.Series:
         """Generates a time series based on the main and residual components."""
@@ -26,7 +38,10 @@ class TimeSeriesGenerator:
 
         # Applying the main components
         result_series = pd.Series(
-            reduce(operation, [mc.values for mc in self.main_components])
+            reduce(
+                lambda x, y: operation(x, y),
+                [mc.generate(self.time_index) for mc in self.main_components],
+            )
         )
 
         # Applying the residual components
