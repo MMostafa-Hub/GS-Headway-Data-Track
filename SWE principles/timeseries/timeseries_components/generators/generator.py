4af84a7b5466b7ba53c21fadb72288cb93a0c999
Mohamed Mostafa <mmostafa.ibra21@gmail.com>
2023-09-03 15:06:27 +0300

    change the signature of Main components into more generic naming

--- SWE principles/timeseries/timeseries_components/generators/generator.py
+++ SWE principles/timeseries/timeseries_components/generators/generator.py
@@ -2,10 +2,7 @@ from abc import ABC, abstractmethod
 import pandas as pd
 
 
-class MainComponent(ABC):
-    def __init__(self):
-        self.time_index: pd.DatetimeIndex = pd.DatetimeIndex()
-
+class Generator(ABC):
     @abstractmethod
     def generate(self, time_index: pd.DatetimeIndex) -> pd.Series:
         """Generates the time series component."""
