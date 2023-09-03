8fa4c2312a54c702daffb818f79cc508843ba0dd
Mohamed Mostafa <mmostafa.ibra21@gmail.com>
2023-09-03 22:57:26 +0300

    Improve the implementation of noise transform function

--- SWE principles/timeseries/timeseries_components/transformers/noise.py
+++ SWE principles/timeseries/timeseries_components/transformers/noise.py
@@ -10,11 +10,5 @@ class NoiseTransformer(Transformer):
 
     def transform(self, time_series: pd.Series) -> pd.Series:
         """Add noise to the time series data."""
-        noise = np.zeros_like(time_series)
-        for i in range(time_series.shape[0]):
-            noise[i] = (
-                np.random.normal(0, abs(time_series[i]) * self.noise_level)
-                if self.noise_level > 0
-                else 0
-            )
+        noise = np.random.normal(0, self.noise_level, size=len(time_series))
         return pd.Series(time_series + noise)
