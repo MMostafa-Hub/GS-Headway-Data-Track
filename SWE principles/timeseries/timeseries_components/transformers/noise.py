7996e0c18fb0260dd7ebd67e52420431014e8834
Mohamed Mostafa <mmostafa.ibra21@gmail.com>
2023-09-03 23:02:15 +0300

    Apply operations on the copy instead of the original object

--- SWE principles/timeseries/timeseries_components/transformers/noise.py
+++ SWE principles/timeseries/timeseries_components/transformers/noise.py
@@ -11,4 +11,4 @@ class NoiseTransformer(Transformer):
     def transform(self, time_series: pd.Series) -> pd.Series:
         """Add noise to the time series data."""
         noise = np.random.normal(0, self.noise_level, size=len(time_series))
-        return pd.Series(time_series + noise)
+        return pd.Series(time_series.copy() + noise)
