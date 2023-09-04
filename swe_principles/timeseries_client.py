6d396a662ed233179e1d844b6feb8979e44a8543
MMostafa-Hub <mmostafa.ibra21@gmail.com>
2023-09-05 01:05:41 +0200

    Dynamically Handle different configuration file and data dump file extensions using cli arguments

--- swe_principles/timeseries_client.py
+++ swe_principles/timeseries_client.py
@@ -1,11 +1,22 @@
 from timeseries.configuration_manager import ConfigurationManager
 from timeseries.timeseries_producer import TimeSeriesProducer
 from timeseries.timeseries_simulator import TimeSeriesSimulator
+import sys
 
 
 def main():
+    cl_args = sys.argv[1:]  # Getting the command line arguments
+    config_path = cl_args[0]  # Getting the path to the configuration file
+    dump_path = cl_args[1]  # Getting the path to the dump file
+
+    # Extract file extension of config_path
+    config_path_ext = config_path.split(".")[-1]
+
+    # Extract file extension of dump_path
+    dump_path_ext = dump_path.split(".")[-1]
+
     # Reading the configuration file
-    time_series_params = ConfigurationManager.yaml("./config.yaml")
+    time_series_params = getattr(ConfigurationManager, config_path_ext)(config_path)
 
     # Creating the time series generator
     time_series_generator = TimeSeriesSimulator(time_series_params)
@@ -14,7 +25,7 @@ def main():
     time_series = time_series_generator.simulate()
 
     # Saving the time series
-    TimeSeriesProducer.csv("./timeseries.csv", time_series)
+    getattr(TimeSeriesProducer, dump_path_ext)(dump_path, time_series)
 
 
 def visualize_timeseries():
