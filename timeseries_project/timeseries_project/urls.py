b5c3d82cad1270ad9c207ff783f1d17690a84aa5
Mohamed Mostafa <moh.mostafaf.ibra@gmail.com>
2023-09-11 18:29:19 +0300

    Change the django project name to avoid any naming collisions

--- timeseries_project/timeseries_project/urls.py
+++ timeseries_project/timeseries_project/urls.py
@@ -1,4 +1,4 @@
-"""timeseries URL Configuration
+"""timeseries_project URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.2/topics/http/urls/
