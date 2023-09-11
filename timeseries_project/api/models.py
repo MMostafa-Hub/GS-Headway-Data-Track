9e9b8c3476ef0077d8d10192e0b14f7d5f505093
Mohamed Mostafa <moh.mostafaf.ibra@gmail.com>
2023-09-11 19:41:49 +0300

    Create a UseCase Model

--- timeseries_project/api/models.py
+++ timeseries_project/api/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class UseCase(models.Model):
+    name = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+    start_date = models.DateTimeField()
+
+    # At least one of the following two fields must be filled
+    # If the two are filled, then the end_date is used
+    end_date = models.DateTimeField()
+    data_size = models.IntegerField()
+
+    producer_types = [
+        ("Additive", "additive"),
+        ("Multiplicative", "multiplicative"),
+    ]
+    producer_type = models.CharField(choices=producer_types)
