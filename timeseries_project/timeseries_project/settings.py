f4b72e3908785f3cdeec27e66951c1082fa60e50
MMostafa-Hub <moh.mostafa.ibra@gmail.com>
2023-10-31 22:26:18 +0200

    Change the db name in settings.py

--- timeseries_project/timeseries_project/settings.py
+++ timeseries_project/timeseries_project/settings.py
@@ -78,7 +78,7 @@ WSGI_APPLICATION = "timeseries_project.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
-        "NAME": "timeseries_db",
+        "NAME": "timeseries_db.sqlite",
     }
 }
 
