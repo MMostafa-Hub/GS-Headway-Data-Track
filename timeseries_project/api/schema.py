d02b26d38ea21b6d3e705bff8d1f19f15f46032f
MMostafa-Hub <moh.mostafa.ibra@gmail.com>
2023-11-27 00:23:11 +0200

    Refactor get_simulators_by query in schema.py

--- timeseries_project/api/schema.py
+++ timeseries_project/api/schema.py
@@ -52,30 +52,59 @@ class Query(graphene.ObjectType):
     # Query to get all simulators
     all_simulators = graphene.List(SimulatorType)
 
-    # Query to get a simulator by name
-    simulator_by_name = graphene.Field(
-        SimulatorType, name=graphene.String(required=True)
-    )
-
-    # Query to get the simulators by sink name
-    simulators_by_sink_name = graphene.List(
-        SimulatorType, sink_name=graphene.String(required=True)
+    # Query to get the simulators by name, sink name, start data, end date, data size, type
+    get_simulators_by = graphene.List(
+        SimulatorType,
+        name=graphene.String(),
+        sink_name=graphene.String(),
+        type=graphene.String(),
+        start_date=graphene.DateTime(),
+        end_date=graphene.DateTime(),
+        data_size=graphene.Int(),
+        interval=graphene.String(),
     )
 
     def resolve_all_simulators(self, info):
         return Simulator.objects.all()
 
-    def resolve_simulator_by_name(self, info, name):
-        try:
-            return Simulator.objects.get(name=name)
-        except Simulator.DoesNotExist:
-            return None
+    def resolve_get_simulators_by(
+        self,
+        info,
+        name=None,
+        sink_name=None,
+        type=None,
+        start_date=None,
+        end_date=None,
+        data_size=None,
+        interval=None,
+    ):
+        """
+        Query to get the simulators by name, sink name, start data, end date, data size, type
+        """
+        simulators = Simulator.objects.all()
 
-    def resolve_simulators_by_sink_name(self, info, sink_name):
-        try:
-            return Simulator.objects.filter(sink_name=sink_name)
-        except Simulator.DoesNotExist:
-            return None
+        if name:
+            simulators = simulators.filter(name=name)
+
+        if sink_name:
+            simulators = simulators.filter(sink_name=sink_name)
+
+        if type:
+            simulators = simulators.filter(type=type)
+
+        if start_date:
+            simulators = simulators.filter(start_date=start_date)
+
+        if end_date:
+            simulators = simulators.filter(end_date=end_date)
+
+        if data_size:
+            simulators = simulators.filter(data_size=data_size)
+
+        if interval:
+            simulators = simulators.filter(interval=interval)
+
+        return simulators
 
 
 schema = graphene.Schema(query=Query)
